4a3953c9394256f2b00e51c0187255e6d2ad4a49
CMH <crai2001@gmail.com>
2020-07-17 15:52:46 -0600

    Updates

---
+++ precache-manifest.7fe257b60c1574ef432b4bb1bfc3072e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac05b200e4503acb1235f5ff046315a6",
+    "url": "/react04r/index.html"
+  },
+  {
+    "revision": "73af4726a0a91285669f",
+    "url": "/react04r/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8338b99ac986449b78a0",
+    "url": "/react04r/static/js/2.73dc716e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react04r/static/js/2.73dc716e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73af4726a0a91285669f",
+    "url": "/react04r/static/js/main.eda651ff.chunk.js"
+  },
+  {
+    "revision": "2732714b15596f80d294",
+    "url": "/react04r/static/js/runtime-main.4d2f1d97.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react04r/static/media/logo.5d5d9eef.svg"
+  }
+]);
